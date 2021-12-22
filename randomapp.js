2073f97ad7547c0c549d4e039d9e3f8ecb8447f2
Sara <78870726+sazdg@users.noreply.github.com>
2021-12-22 12:34:34 +0100

    new font + new subjects

--- randomapp.js
+++ randomapp.js
@@ -44,17 +44,33 @@ app.controller("ctrl", function ($scope, $http) {
 			//random results FUNCTION
 			//$scope.numero = Math.floor(Math.random() * ($scope.len + 1));
 
-			$scope.sub = ["subject 1", "subject 2", "subject 3", "subject 4", "subject 5"];
+			
+			//local storage - session/cookies
+			var a = localStorage.setItem("sub1", "Subject 1");
+			var b = localStorage.setItem("sub2", "Subject 2");
+			var c = localStorage.setItem("sub3", "Subject 3");
+			var d = localStorage.setItem("sub4", "Subject 4");
+			var e = localStorage.setItem("sub5", "Subject 5");
+			//TO DO: fare apparire i risultati registrati nella sessione quando si chiude e si riapre la finestra
+
+			$scope.sub = ["Subject 1", "Subject 2", "Subject 3", "Subject 4", "Subject 5"];
+			//subjects locked, saved
 			$scope.save = ["", "", "", "", ""];
+
 
 			$scope.random = function () {
 				for (let i = 0; i < ($scope.selected); i++) {
 					if ($scope.lock[i]) {
 						//if true, display a new subject
-						$scope.sub[i] = response.data[Math.floor(Math.random() * ($scope.len))];
+						$scope.sub[i] = response.data[Math.floor(Math.random() * ($scope.len))]; 
+						localStorage.setItem("sub" + (i + 1), $scope.sub[i]);
+						//controllo
+						console.log("session: " + (i + 1) + " " + localStorage.getItem("sub" + (i + 1)));
+
 					} else {
 						//save result and skip
 						$scope.save[i] = $scope.sub[i];
+						localStorage.setItem("sub" + (i + 1), $scope.sub[i]);
 					}
 				}
 			};
@@ -67,5 +83,7 @@ app.controller("ctrl", function ($scope, $http) {
 
 	$scope.reset = function () {
 		window.location.reload();
+		window.localStorage.clear();
 	};
+
 });
